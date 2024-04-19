58931b6a45e157a6c6a8e4589cc6b8280da8664c
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-19 10:54:19 +0530

    add rows at same position bug

--- assets/script/customPageView.ts
+++ assets/script/customPageView.ts
@@ -1,14 +1,29 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, instantiate, Label, Node, Prefab, ScrollView, UITransform, Vec3 } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('customPageView')
+@ccclass("customPageView")
 export class customPageView extends Component {
-    start() {
+    @property({ type: ScrollView })
+    scrollView: ScrollView | null = null;
 
+    // @property({type:Node})
+    // rowParent: Node = null;
+
+    @property({ type: Prefab })
+    rowPrefab: Prefab = null;
+
+    protected onLoad(): void {
+        this.scrollView.node.on("scroll-to-top", this.callback, this);
+        for (let i = 0; i < 50; i++) {
+            const row = instantiate(this.rowPrefab);
+
+            this.scrollView.content.addChild(row);
+            console.log(this.scrollView.content.position);
+        }
     }
 
-    update(deltaTime: number) {
-        
-    }
+    callback(scrollView: ScrollView) {}
+    start() {}
+
+    update(deltaTime: number) {}
 }
-
