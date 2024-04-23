b246d1246407385cba5f095667850f1c1ec669ec
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-23 15:53:48 +0530

    implemented go to page index using edit box, working properly

--- assets/script/profileScript/customProfile.ts
+++ assets/script/profileScript/customProfile.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, instantiate, Node, PageView, Prefab } from "cc";
+import { _decorator, Component, EditBoxComponent, instantiate, Label, Node, PageView, Prefab } from "cc";
 import { CreateCustomProfileContent } from "./CreateCustomProfileContent";
 const { ccclass, property } = _decorator;
 
@@ -18,17 +18,40 @@ export class customProfile extends Component {
     @property({ type: PageView })
     pageView: PageView = null;
 
+    @property({ type: EditBoxComponent })
+    editBox: EditBoxComponent = null;
+
+    @property({ type: Node })
+    editBoxTextLabel: Node = null;
+
     start() {
         for (let i = 0; i < 5; i++) {
             const profile = instantiate(this.singleProfile);
-            console.log(profile);
-            console.log("component", profile.getComponent(CreateCustomProfileContent) ? "null" : "karan");
+
             profile
                 .getComponent(CreateCustomProfileContent)
                 .setCustomContent(this.profileArray[i][0].toString(), this.profileArray[i][1].toString());
             this.pageView.addPage(profile);
             // this.pageView.content.addChild(profile);
         }
+        // this.editBox.node.on(EditBoxComponent.EventType.EDITING_DID_BEGAN, () => console.log("edit began", this));
+        // this.editBox.node.on(EditBoxComponent.EventType.TEXT_CHANGED, () => console.log("text changed"), this);
+        this.editBox.node.on(
+            EditBoxComponent.EventType.EDITING_DID_ENDED,
+            () => {
+                const num = Number(this.editBoxTextLabel.getComponent(Label).string);
+                console.log("edit end");
+                console.log(this.editBoxTextLabel.getComponent(Label).string);
+                if (num > 4) {
+                    console.error("Enter between range 0 to 4");
+                } else this.pageView.scrollToPage(num);
+
+                if (num == this.pageView.getCurrentPageIndex()) {
+                    console.log(`you are already on the page index ${num}`);
+                }
+            },
+            this
+        );
     }
 
     update(deltaTime: number) {}
