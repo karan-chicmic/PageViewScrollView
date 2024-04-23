df5a464e59619ae2d709c4e9403e6d4fc4731e36
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-23 11:54:24 +0530

    vertical page view not working properly

--- assets/script/pageViewScript/curstomPageView.ts
+++ assets/script/pageViewScript/curstomPageView.ts
@@ -1,28 +1,21 @@
-import { _decorator, Component, instantiate, Label, Node, PageView, Prefab, UITransform } from "cc";
+import { _decorator, Component, instantiate, Label, Node, PageView, Prefab, UITransform, Widget } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass("pageViewScript")
-export class pageViewScript extends Component {
-    // @property({ type: Node })
-    // page1: Node = null;
-    // @property({ type: Node })
-    // page: Node = null;
-    // @property({ type: Node })
-    // page3: Node = null;
+@ccclass("curstomPageView")
+export class curstomPageView extends Component {
+    @property({ type: Prefab })
+    page1Prefab: Prefab = null;
 
     @property({ type: PageView })
     pageView: PageView = null;
-
-    @property({ type: Prefab })
-    page1Prefab: Prefab;
-
     start() {
         for (let i = 1; i <= 60; i++) {
             const row = instantiate(this.page1Prefab);
 
-            row.getComponent(Label).string = i.toString() + "                   ";
-            row.getComponent(UITransform).height = 600;
+            row.getComponent(Label).string = i.toString();
+
             this.pageView.addPage(row);
+
             // this.pageView.content.addChild(row);
 
             this.pageView.node.on("page-turning", () => console.log("page turned"), this);
