fa4b12a1fd347a15f67352a9cf0a109a0be06bb3
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-23 12:41:31 +0530

    vertical page view bug, not working properly

--- assets/script/pageViewScript/curstomPageView.ts
+++ assets/script/pageViewScript/curstomPageView.ts
@@ -3,27 +3,38 @@ const { ccclass, property } = _decorator;
 
 @ccclass("curstomPageView")
 export class curstomPageView extends Component {
-    @property({ type: Prefab })
-    page1Prefab: Prefab = null;
+    // @property({ type: Node })
+    // page1: Node = null;
+    // @property({ type: Node })
+    // page2: Node = null;
+    // @property({ type: Node })
+    // page3: Node = null;
+    // @property({ type: Node })
+    // page4: Node = null;
+    // @property({ type: Node })
+    // page5: Node = null;
+
+    @property({ type: [Node] })
+    pageArray: [Node] | [] = [];
 
     @property({ type: PageView })
     pageView: PageView = null;
+
+    @property({ type: Prefab })
+    page1Prefab: Prefab = null;
     start() {
-        for (let i = 1; i <= 60; i++) {
-            const row = instantiate(this.page1Prefab);
-
-            row.getComponent(Label).string = i.toString();
-
-            this.pageView.addPage(row);
-
-            // this.pageView.content.addChild(row);
-
-            this.pageView.node.on("page-turning", () => console.log("page turned"), this);
-            // this.pageView.setCurrentPageIndex(i - 1);
-
-            // this.node.addChild(row);
+        for (let k = 0; k < this.pageArray.length; k++) {
+            const row = this.pageArray[k];
+            for (let j = 0; j < 15; j++) {
+                const labelInstance = instantiate(this.page1Prefab);
+                labelInstance.getComponent(Label).string = (k + 1).toString();
+                row.addChild(labelInstance);
+            }
+            this.pageView.node.on("page-turning", () => this.pageTurnCallback(k), this);
         }
     }
-
+    pageTurnCallback(currPageIndex) {
+        this.pageView.scrollToPage(currPageIndex + 1);
+    }
     update(deltaTime: number) {}
 }
