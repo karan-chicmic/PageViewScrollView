729794a1ccd281742ef5a3504375802ca3d645e9
Abhishek Rawat <abhishek.rawat@chicmic.co.in>
2024-04-26 17:45:38 +0530

    only lag bug pending rest is done no other error

--- assets/script/scrollViewScript/customPageView.ts
+++ assets/script/scrollViewScript/customPageView.ts
@@ -20,7 +20,7 @@ export class customPageView extends Component {
     dataArray = [
         ["karan ", 1000, 1, 5],
         ["prabh", 950, 2, 5],
-        ["harjot", 900, 3, 5],
+        ["saniya", 900, 3, 5],
         ["anmol", 870, 4, 4],
         ["santa", 840, 5, 4],
         ["rishi", 800, 6, 4],
@@ -28,11 +28,6 @@ export class customPageView extends Component {
         ["navjot", 749, 8, 4],
         ["deepak", 730, 9, 3],
         ["rupinder", 729, 10, 3],
-        ["navneet", 720, 11, 3],
-        ["jashan", 689, 12, 2],
-        ["harsh", 500, 13, 2],
-        ["saniya", 480, 14, 2],
-        ["carry", 400, 15, 2],
     ];
     @property({ type: ScrollView })
     scrollView: ScrollView | null = null;
